4dc737b41778310ee5dddb1641ca669f16282819
Simon S W Liu <simon103484947@gmail.com>
2016-07-26 14:04:44 +0800

    ionic入门教程第五课-举例子说明异步回调$q及$q在项目中的用法
    http://blog.csdn.net/onil_chen/article/details/51099443

--- www/js/controllers/ChatsCtrl.js
+++ www/js/controllers/ChatsCtrl.js
@@ -1,35 +1,41 @@
-  //.controller('ChatsCtrl', function($scope, Chats) {
-  //  // With the new view caching in Ionic, Controllers are only called
-  //  // when they are recreated or on app start, instead of every page change.
-  //  // To listen for when this page is active (for example, to refresh data),
-  //  // listen for the $ionicView.enter event:
-  //  //
-  //  //$scope.$on('$ionicView.enter', function(e) {
-  //  //});
-  //
-  //  $scope.chats = Chats.all();
-  //  $scope.remove = function(chat) {
-  //    Chats.remove(chat);
-  //  };
-  //})
-  //修改后的文件如下
-  define(['app'], function (app) {
-    'use strict';
-    function ctrl($scope, Chats) {
-      //With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
+//.controller('ChatsCtrl', function($scope, Chats) {
+//  // With the new view caching in Ionic, Controllers are only called
+//  // when they are recreated or on app start, instead of every page change.
+//  // To listen for when this page is active (for example, to refresh data),
+//  // listen for the $ionicView.enter event:
+//  //
+//  //$scope.$on('$ionicView.enter', function(e) {
+//  //});
+//
+//  $scope.chats = Chats.all();
+//  $scope.remove = function(chat) {
+//    Chats.remove(chat);
+//  };
+//})
+//修改后的文件如下
+define(['app'], function (app) {
+  'use strict';
+  function ctrl($scope, Chats) {
+    //With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-        $scope.chats = Chats.all();
-        $scope.remove = function(chat) {
-          Chats.remove(chat);
-        };
-    }
-    ctrl.$inject = ['$scope', 'Chats'];
-    app.registerController('ChatsCtrl',ctrl);
-    // return ctrl;
-  });
+    // $scope.chats = Chats.all();
+    Chats.all().then(function (data) {
+      $scope.chats=data
+    }).finally(function () {
+      
+    });
+
+    $scope.remove = function(chat) {
+      Chats.remove(chat);
+    };
+  }
+  ctrl.$inject = ['$scope', 'Chats'];
+  app.registerController('ChatsCtrl',ctrl);
+  // return ctrl;
+});
